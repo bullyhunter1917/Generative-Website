724a86f784937c23fb164dec5b5a46e414fd894d
Michał Mróz <michamroz77@gmail.com>
2024-04-24 12:50:41 +0200

    added account and storage page

--- WebApp/views.py
+++ WebApp/views.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template
+from flask_login import current_user, login_required
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    return render_template('home.html', user=current_user)
+
+@views.route('/storage', methods=['GET', 'POST'])
+@login_required
+def storage():
+    return render_template('storage.html', user=current_user)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from WebApp import create_app
 
-app = create_app()
-
 if __name__=='__main__':
+    app = create_app()
+
     app.run(debug=True)
