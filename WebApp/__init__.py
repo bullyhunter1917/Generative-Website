0bad002d437a9e49cebc52eb66af22668064b530
Michał Mróz <michamroz77@gmail.com>
2024-05-22 19:54:10 +0200

    Started implementation of celery queue for generating pictures

--- WebApp/__init__.py
+++ WebApp/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from celery import Celery, Task
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 import secrets
@@ -7,10 +8,30 @@ from os import path
 db = SQLAlchemy()
 DB_NAME = 'database.db'
 
+def celery_init_app(app: Flask) -> Celery:
+    class FlaskTask(Task):
+        def __call__(self, *args: object, **kwargs: object) -> object:
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery_app = Celery(app.name, task_cls=FlaskTask)
+    celery_app.config_from_object(app.config["CELERY"])
+    celery_app.set_default()
+    app.extensions["celery"] = celery_app
+    return celery_app
+
 def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static')
     app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config.from_mapping(
+        CELERY=dict(
+            broker_url="redis://localhost",
+            result_backend="redis://localhost",
+            task_ignore_result=True,
+        ),
+    )
+
     db.init_app(app)
 
     from .views import views
@@ -31,6 +52,11 @@ def create_app():
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
+
+    app.config.from_prefixed_env()
+    celery_init_app(app)
+
+    # app.config['CELERY'] = celery
 
     return app
 
--- WebApp/views.py
+++ WebApp/views.py
@@ -1,13 +1,34 @@
-from flask import Blueprint, render_template
+from celery import shared_task
+from flask import Blueprint, render_template, request, current_app
 from flask_login import current_user, login_required
 
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
+    print(request.method)
+    if request.method == 'POST':
+
+        text_input = request.form.get('inputText')
+        picture_input = request.form.get('imageInput')
+
+        print(text_input)
+
+        result = generate_picture.delay(3, 4)
+        print(result.get())
+        print(result.status)
+        return {"result_id": result.id}
+
     return render_template('home.html', user=current_user)
+
+
 
 @views.route('/storage', methods=['GET', 'POST'])
 @login_required
 def storage():
     return render_template('storage.html', user=current_user)
+
+@shared_task(ignore_result=False)
+def generate_picture(x, y) -> int:
+
+    return x+y
